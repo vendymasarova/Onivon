111d4352a301e0c5163c03733feb67cf7a6246f5
vendymasarova <vendymasarova@seznam.cz>
2022-05-06 16:31:33 +0200

    mobile nav

--- src/scripts/mainNav.js
+++ src/scripts/mainNav.js
@@ -1,40 +1,62 @@
-
-
 const mainNav = () => {
 
   const hamburger = document.querySelector('.js--hamburger');
   const nav = document.querySelector('.o-header__colapse');
   const brand = document.querySelector('.a-brand');
   const itemWithSubmenu = document.querySelector('.js--has-submenu');
+  const submenu = document.querySelector('.js--submenu');
+  const body = document.querySelector('body');
+
+   const toggleNavbar = () => {
+    hamburger.classList.toggle('is-active');
+      nav.classList.toggle('open');
+
+      if(nav.classList.contains('open')) {
+        brand.style.opacity = '0';
+        window.scrollTo(0, 0);
+        body.style.overflow = 'hidden';
+      }
+
+      else {
+        brand.style.opacity = '1';
+        body.style.overflow = 'auto';
+        submenu.classList.remove('open');
+      }
+   }
+
+   const onPageResize = () => {
+    if (window.innerWidth > 902) {
+      nav.classList.remove('open');
+      hamburger.classList.remove('is-active');
+      body.style.overflow = 'visible';
+      brand.style.opacity = '1';
+    }
+   }
+
+   const openSubmenu = () => {
+
+    let nextSibling = itemWithSubmenu.nextElementSibling;
+    nextSibling.classList.toggle('open');
+   }
 
   if(hamburger){
 
     hamburger.addEventListener('click', () => {
+      toggleNavbar();
 
-    hamburger.classList.toggle('is-active');
-    nav.classList.toggle('open');
-
-    if(nav.classList.contains('open')) {
-      brand.style.opacity = '0';
-      window.scrollTo(0, 0);
-      body.style.overflow = "hidden";
-    }
-
-    else {
-      brand.style.opacity = '1';
-      body.style.overflow = "auto";
-    }
-  })
-
-  if(itemWithSubmenu) {
-
-    itemWithSubmenu.addEventListener(('click'), () => {
-
-      let nextSibling = itemWithSubmenu.nextElementSibling;
-
-      nextSibling.classList.toggle('open');
     })
+
+    window.addEventListener('resize', function() {
+      onPageResize();
+    });
+
+    if(itemWithSubmenu) {
+
+      itemWithSubmenu.addEventListener(('click'), () => {
+        openSubmenu();
+
+      })
+    }
   }
-}
 }
 export default mainNav;
--- www/js/bundle.esm.js
+++ www/js/bundle.esm.js
@@ -5,26 +5,49 @@ const mainNav = () => {
   const nav = document.querySelector('.o-header__colapse');
   const brand = document.querySelector('.a-brand');
   const itemWithSubmenu = document.querySelector('.js--has-submenu');
+  const submenu = document.querySelector('.js--submenu');
+  const body = document.querySelector('body');
+
+  const toggleNavbar = () => {
+    hamburger.classList.toggle('is-active');
+    nav.classList.toggle('open');
+
+    if (nav.classList.contains('open')) {
+      brand.style.opacity = '0';
+      window.scrollTo(0, 0);
+      body.style.overflow = 'hidden';
+    } else {
+      brand.style.opacity = '1';
+      body.style.overflow = 'auto';
+      submenu.classList.remove('open');
+    }
+  };
+
+  const onPageResize = () => {
+    if (window.innerWidth > 902) {
+      nav.classList.remove('open');
+      hamburger.classList.remove('is-active');
+      body.style.overflow = 'visible';
+      brand.style.opacity = '1';
+    }
+  };
+
+  const openSubmenu = () => {
+    let nextSibling = itemWithSubmenu.nextElementSibling;
+    nextSibling.classList.toggle('open');
+  };
 
   if (hamburger) {
     hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('is-active');
-      nav.classList.toggle('open');
-
-      if (nav.classList.contains('open')) {
-        brand.style.opacity = '0';
-        window.scrollTo(0, 0);
-        body.style.overflow = "hidden";
-      } else {
-        brand.style.opacity = '1';
-        body.style.overflow = "auto";
-      }
+      toggleNavbar();
+    });
+    window.addEventListener('resize', function () {
+      onPageResize();
     });
 
     if (itemWithSubmenu) {
       itemWithSubmenu.addEventListener('click', () => {
-        let nextSibling = itemWithSubmenu.nextElementSibling;
-        nextSibling.classList.toggle('open');
+        openSubmenu();
       });
     }
   }
