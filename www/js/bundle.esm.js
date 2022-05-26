1a47973d7435760f5c604a4e7b6e7b4a250ee841
vendymasarova <vendymasarova@seznam.cz>
2022-05-26 18:26:15 +0200

    feat: gsap animation

--- www/js/bundle.esm.js
+++ www/js/bundle.esm.js
@@ -22362,34 +22362,84 @@ const smoothImageMove = () => {
 gsapWithCSS.registerPlugin(ScrollTrigger, Timeline);
 
 const timelineAnimation = () => {
-  const lines = document.querySelectorAll('.js--line');
+  const timelineWrapper = document.querySelector('.js--timeline');
+  const timelineSection = document.querySelector('.js--timeline-section');
+  const lines = document.querySelectorAll('.js--timeline-line');
   const linesContent = document.querySelectorAll('.js--timeline-content');
+  const contentBoxTop = document.querySelectorAll('.js--timeline-content--top');
+  const contentBoxBottom = document.querySelectorAll('.js--timeline-content--bottom');
   const dottedLine = document.querySelector('.js--dotted-line');
-  gsapWithCSS.timeline({
-    scrollTrigger: {
-      trigger: '.js--timelineAnimation',
-      scrub: true,
-      pin: true,
-      start: 'top top',
-      end: '+=100%',
-      once: true
-    }
-  }).from(lines, {
-    scaleX: 0,
-    stagger: 0.6,
-    transformOrigin: 'left center'
-  }).from(linesContent, {
-    scaleX: 1,
-    opacity: 0,
-    stagger: 0.6
-  }, '<').to(dottedLine, {
-    opacity: 1
-  });
-  gsapWithCSS.utils.toArray('.js--timeline-content').forEach(content => {
-    gsapWithCSS.set(content, {
-      opacity: 1
+
+  if (timelineWrapper) {
+    const topBoxArray = [];
+    const bottomBoxArray = [];
+    contentBoxTop.forEach(box => {
+      topBoxArray.push(box.clientHeight);
     });
-  });
+    const maxTopBoxHeight = Math.max(...topBoxArray);
+    contentBoxBottom.forEach(box => {
+      bottomBoxArray.push(box.clientHeight);
+    });
+    const maxBottomBoxHeight = Math.max(...bottomBoxArray);
+    const timelineWrapperHeight = maxTopBoxHeight + maxBottomBoxHeight + 176;
+    timelineWrapper.style.height = `${timelineWrapperHeight + 'px'}`;
+    console.log(timelineWrapperHeight);
+    const sectionHeight = parseInt(timelineSection.clientHeight + timelineWrapperHeight);
+    console.log(sectionHeight);
+    ScrollTrigger.matchMedia({
+      "(min-height: 1000px)": function () {
+        // gsap.set(lines, {
+        //   opacity: 1
+        // })
+        // // gsap.set(lines, {
+        // //   scaleX: 0,
+        // //   opacity: 0,
+        // // })
+        // // gsap.set(linesContent, {
+        // //   opacity: 0
+        // // })
+        const anim = gsapWithCSS.timeline({
+          scrollTrigger: {
+            trigger: '.js--timeline',
+            scrub: 1,
+            pin: true,
+            start: 'center center',
+            end: 'bottom 20%',
+            once: true,
+            // markers: true,
+            defaults: {
+              duration: 5
+            },
+            onLeave: self => {
+              // let scroll = self.scroll();
+              self.kill(); // console.log(scroll);
+              // self.scroll(scroll);
+
+              anim.progress(1);
+            } // anticipatePin: 1,
+
+          }
+        }).fromTo(lines, {
+          scaleX: 0,
+          opacity: 0
+        }, {
+          scaleX: 1,
+          opacity: 1,
+          stagger: 0.8,
+          transformOrigin: 'left center'
+        }).fromTo(linesContent, {
+          opacity: 0
+        }, {
+          opacity: 1,
+          stagger: 0.8
+        }, '<').fromTo(dottedLine, {
+          opacity: 0
+        }, {
+          opacity: 1
+        });
+      }
+    });
+  }
 };
 
 window.addEventListener('load', function () {
